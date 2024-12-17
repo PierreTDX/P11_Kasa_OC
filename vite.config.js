96e5632fd857bc81b1571e7edc51067e509190a7
PierreTDX <pierre.tondeux@gmail.com>
2024-12-17 19:03:40 +0100

    config vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/P11_Kasa_OC/",
   plugins: [react()],
   server: {
     host: true,
