2769ae07d82584dffdae6326ddcd4755212f45cb
PierreTDX <pierre.tondeux@gmail.com>
2024-12-17 09:57:06 +0100

    Ajout isActive aux liens du header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import './header.scss'
 import MainLogo from '../Logo'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 function Header() {
@@ -13,12 +13,12 @@ function Header() {
           </div>
           <nav>
             <ul className='inf768'>
-              <li><Link to="/">ACCUEIL</Link></li>
-              <li><Link to="/about">A PROPOS</Link></li>
+              <li><NavLink to="/" className={({ isActive }) => (isActive ? 'activeLink' : '')}>ACCUEIL</NavLink></li>
+              <li><NavLink to="/about" className={({ isActive }) => (isActive ? 'activeLink' : '')}>A PROPOS</NavLink></li>
             </ul>
             <ul className='sup768'>
-              <li><Link to="/">Accueil</Link></li>
-              <li><Link to="/about">A Propos</Link></li>
+              <li><NavLink to="/" className={({ isActive }) => (isActive ? 'activeLink' : '')}>Accueil</NavLink></li>
+              <li><NavLink to="/about" className={({ isActive }) => (isActive ? 'activeLink' : '')}>A Propos</NavLink></li>
             </ul>
           </nav>
         </header>
