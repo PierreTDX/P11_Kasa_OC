eda66e70e0484f698b8da3fb85c39110ffaae4f3
PierreTDX <pierre.tondeux@gmail.com>
2024-12-17 19:35:45 +0100

    ajout attribut "end" pour le NavLink de Home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,11 +13,11 @@ function Header() {
           </div>
           <nav>
             <ul className='inf768'>
-              <li><NavLink to="/P11_Kasa_OC/" className={({ isActive }) => (isActive ? 'activeLink' : '')}>ACCUEIL</NavLink></li>
+              <li><NavLink to="/P11_Kasa_OC/" end className={({ isActive }) => (isActive ? 'activeLink' : '')}>ACCUEIL</NavLink></li>
               <li><NavLink to="/P11_Kasa_OC/about" className={({ isActive }) => (isActive ? 'activeLink' : '')}>A PROPOS</NavLink></li>
             </ul>
             <ul className='sup768'>
-              <li><NavLink to="/P11_Kasa_OC/" className={({ isActive }) => (isActive ? 'activeLink' : '')}>Accueil</NavLink></li>
+              <li><NavLink to="/P11_Kasa_OC/" end className={({ isActive }) => (isActive ? 'activeLink' : '')}>Accueil</NavLink></li>
               <li><NavLink to="/P11_Kasa_OC/about" className={({ isActive }) => (isActive ? 'activeLink' : '')}>A Propos</NavLink></li>
             </ul>
           </nav>
